7ea6ca3406c1c4a1e111f95659e7664614175cc9
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-03 19:18:49 +0700

    menambahkan file constructor.php

--- constructor.php
+++ constructor.php
@@ -20,10 +20,13 @@
 
 	$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 	$produk2 = new Produk("Uncharted", "Nail Druckmann", "Sony Computer", 250000);
+	$produk3 = new Produk("Dragon Ball");
 
 	echo "Komik : " . $produk1->getLabel();
 	echo "<br>";
 	echo "Game : " . $produk2->getLabel();
+	echo "<br>";
+	var_dump($produk3);
 
 
  ?>
