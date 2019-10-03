2eacea2aaf95071883f236cc9a546dab985d3b15
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-03 18:57:58 +0700

    menambahkan isi ke dalam parameter constructor

--- constructor.php
+++ constructor.php
@@ -1,43 +1,29 @@
 <?php 
 
 	class Produk {
-		public 	$judul = "judul",
+		public 	$judul = "judul", //property dari kelas
 				$penulis = "penulis",
 				$penerbit = "penerbit",
 				$harga = 0;
+
+		public function __construct( $judul, $penulis, $penerbit, $harga ) { // variabel lokal
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+		}
 
 		public function getLabel() {
 			return "$this->penulis, $this->penerbit"; // method
 		}
 	}
 
-	// $produk1 = new Produk();
-	// $produk1->judul = "Naruto"; // ditimpa
-	// var_dump($produk1);
-
-	// $produk2 = new Produk();
-	// $produk2->judul = "Ucharted"; // ditimpa
-	// $produk2->tambahProperty = "hahaha";
-	// var_dump($produk2);
-
-	$produk3 = new Produk();
-	$produk3->judul = "Naruto"; // ditimpa
-	$produk3->penulis = "Masashi Kishimoto";
-	$produk3->penerbit = "Shonen Jump";
-	$produk3->harga = 30000;
-
-	// echo "Komik : $produk3->penulis, $produk3->penerbit";
-
-
-	$produk4 = new Produk();
-	$produk4->judul = "Uncharted"; // ditimpa
-	$produk4->penulis = "Nail Druckmann";
-	$produk4->penerbit = "Sony Computer";
-	$produk4->harga = 250000;
+	$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",  30000);
+	$produk2 = new Produk("Uncharted", "Nail Druckmann", "Sony Computer, 250000);
 
 	echo "Komik : " . $produk3->getLabel();
 	echo "<br>";
 	echo "Game : " . $produk4->getLabel();
 
-	
+
  ?>
