4ae2351b9be2fba90cbfcde9342ca9718d491e23
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-03 16:45:21 +0700

    menambahkan object kedua

--- produk.php
+++ produk.php
@@ -1,14 +1,19 @@
 <?php 
 
 	class Produk {
-		public 	$judul,
-				$penulis,
-				$penerbit,
-				$harga;
+		public 	$judul = "judul",
+				$penulis = "penulis",
+				$penerbit = "penerbit",
+				$harga = 0;
 	}
 
 	$produk1 = new Produk();
-
+	$produk1->judul = "Naruto"; // ditimpa
 	var_dump($produk1);
+
+	$produk2 = new Produk();
+	$produk2->judul = "Ucharted"; // ditimpa
+	$produk2->tambahProperty = "hahaha";
+	var_dump($produk2);
 
  ?>
