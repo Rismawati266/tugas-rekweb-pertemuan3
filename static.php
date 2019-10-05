c4924c37f33b28ce0ad94b03ce35763388d0d36b
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 02:01:35 +0700

    menampilkan halo dua kali

--- static.php
+++ static.php
@@ -4,13 +4,15 @@
 		public static $angka = 1; // variabel/property property bernama angka, property tsb punya keyword static
 
 		public static function halo() { // method
-			return "Halo.";
+			return "Halo." . self::$angka . " kali.";
 		}
 	}
 
 	echo ContohStatic::$angka; // mencetak angka 1. mengakses property atau method keyword static
 	echo "<br>";
 	echo ContohStatic::halo(); // tand kurung yaitu method
+	echo "<hr>";
+	echo ContohStatic::halo();
 
 
  ?>
