90a4d4754ce0ec93592d2341b72115420bb0cb1c
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-05 18:25:18 +0700

    menambahkan file overriding.php

--- overriding.php
+++ overriding.php
@@ -65,11 +65,10 @@
 	$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 	$produk2 = new Game("Uncharted", "Nail Druckmann", "Sony Computer", 250000, 50);
 
-
 	echo $produk1->getInfoProduk();
 	echo "<br>";
 	echo $produk2->getInfoProduk();
-	
+
 
 
  ?>
