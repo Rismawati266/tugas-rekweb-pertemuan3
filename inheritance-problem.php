19d35d8298fb22e1237f88de305fd7d203d4cb39
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-05 13:59:37 +0700

    menambahkan komik di produk1 dan game di produk2

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -29,8 +29,8 @@
 		}
 	}
 
-	$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
-	$produk2 = new Produk("Uncharted", "Nail Druckmann", "Sony Computer", 250000, 0, 50);
+	$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+	$produk2 = new Produk("Uncharted", "Nail Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
 // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
 // Game : Uncharted | Nail Druckmann, Sony Computer
