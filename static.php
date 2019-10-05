130ce3b697eb1bd899d644cd4d0c2beddbaf1700
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 01:45:31 +0700

    membuat static property dan static method

--- static.php
+++ static.php
@@ -1,7 +1,11 @@
 <?php 
 
 	class ContohStatic {
-		
+		public static $angka = 1; // variabel/property property bernama angka, property tsb punya keyword static
+
+		public static function halo() { // method
+			return "Halo.";
+		}
 	}
 
  ?>
