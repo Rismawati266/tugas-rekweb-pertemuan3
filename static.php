52c6323da41288ffb9d62de8b48d4358e8c01ce8
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 02:11:39 +0700

    menambahkan kelas contoh

--- static.php
+++ static.php
@@ -1,18 +1,26 @@
 <?php 
 
-	class ContohStatic {
-		public static $angka = 1; // variabel/property property bernama angka, property tsb punya keyword static
+	// class ContohStatic {
+		// public static $angka = 1; // variabel/property property bernama angka, property tsb punya keyword static
 
-		public static function halo() { // method
-			return "Halo." . self::$angka++. " kali.";
+		// public static function halo() { // method
+			// return "Halo." . self::$angka++. " kali.";
+		// }
+	// }
+
+	// echo ContohStatic::$angka; // mencetak angka 1. mengakses property atau method keyword static
+	// echo "<br>";
+	// echo ContohStatic::halo(); // tand kurung yaitu method
+	// echo "<hr>";
+	// echo ContohStatic::halo();
+
+	class Contoh {
+		public $angka = 1;
+
+		public functio halo() {
+			return "Halo" . $this->angka . " kali.";
 		}
 	}
-
-	echo ContohStatic::$angka; // mencetak angka 1. mengakses property atau method keyword static
-	echo "<br>";
-	echo ContohStatic::halo(); // tand kurung yaitu method
-	echo "<hr>";
-	echo ContohStatic::halo();
 
 
  ?>
