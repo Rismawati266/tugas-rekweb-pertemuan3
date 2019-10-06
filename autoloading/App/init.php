2c30600cc3773a79dd0c52756a125b92f810e4e4
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 23:14:10 +0700

    menambahkan file int.php dan mengubah isi file index.php

--- autoloading/App/init.php
+++ autoloading/App/init.php
@@ -1,9 +1,13 @@
 <?php 
 
-	require_once 'Produk/InfoProduk.php';
-	require_once 'Produk/Produk.php';
-	require_once 'Produk/Komik.php'; // untuk satu kelas komik
-	require_once 'Produk/Game.php';
-	require_once 'Produk/CetakInfoProduk.php';
+	// require_once 'Produk/InfoProduk.php';
+	// require_once 'Produk/Produk.php';
+	// require_once 'Produk/Komik.php'; // untuk satu kelas komik
+	// require_once 'Produk/Game.php';
+	// require_once 'Produk/CetakInfoProduk.php';
+
+	spl_autoload_register(function( $class ) {
+		require_once __DIR__ . '/Produk/' . $class . '.php';
+	});
 
  ?>
