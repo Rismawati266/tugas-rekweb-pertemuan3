6074b93f13c618419f4649606facfeeefa7f6e9e
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 15:11:58 +0700

    merapihkan kodingan di file Produk.php dan Komik.php

---
+++ autoloading/App/Produk/Komik.php
@@ -0,0 +1,25 @@
+<?php 
+
+	class Komik extends Produk implements InfoProduk { // kelas child // perluasan fungsionalitas ada di kelas2 ada di kelas child yaitu jumlah halaman dan waktu main 
+	public $jmlHalaman;
+
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0 ) {
+
+		parent::__construct( $judul, $penulis, $penerbit, $harga );
+		$this->jmlHalaman = $jmlHalaman;
+	}
+
+	public function getInfo() {
+		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+		return $str;
+	}
+
+	public function getInfoProduk() {
+		$str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+		return $str;
+	}
+	
+	}
+
+ ?>
--- autoloading/App/Produk/Produk.php
+++ autoloading/App/Produk/Produk.php
@@ -1,64 +1,64 @@
 <?php 
 
-		abstract class Produk {
-		protected $judul, //property dari kelas
-				$penulis,
-				$penerbit,
-				$harga,
-				$diskon = 0; // persentasi dari sebuah produk
+	abstract class Produk {
+	protected $judul, //property dari kelas
+		$penulis,
+		$penerbit,
+		$harga,
+		$diskon = 0; // persentasi dari sebuah produk
 
-		public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) { // variabel lokal
-			$this->judul = $judul;
-			$this->penulis = $penulis;
-			$this->penerbit = $penerbit;
-			$this->harga = $harga;
-		}
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) { // variabel lokal
+		$this->judul = $judul;
+		$this->penulis = $penulis;
+		$this->penerbit = $penerbit;
+		$this->harga = $harga;
+	}
 
-		public function setJudul( $judul ) {
-			$this->judul = $judul;
-		}
+	public function setJudul( $judul ) {
+		$this->judul = $judul;
+	}
 
-		public function getJudul() {
-			return $this->judul;
-		}
+	public function getJudul() {
+		return $this->judul;
+	}
 
-		public function setPenulis ( $penulis ) {
-			$this->penulis = $penulis;
-		}
+	public function setPenulis ( $penulis ) {
+		$this->penulis = $penulis;
+	}
 
-		public function getPenulis() {
-			return $this->penulis;
-		}
+	public function getPenulis() {
+		return $this->penulis;
+	}
 
-		public function setPenerbit ( $penerbit ) {
-			$this->penerbit = $penerbit;
-		}
+	public function setPenerbit ( $penerbit ) {
+		$this->penerbit = $penerbit;
+	}
 
-		public function getPenerbit() {
-			return $this->penerbit;
-		}
+	public function getPenerbit() {
+		return $this->penerbit;
+	}
 
-		public function setDiskon( $diskon ) {
-			$this->diskon = $diskon;
-		}
+	public function setDiskon( $diskon ) {
+		$this->diskon = $diskon;
+	}
 
-		public function getDiskon() {
-			return $this->diskon;
-		}
+	public function getDiskon() {
+		return $this->diskon;
+	}
 
-		public function setHarga( $harga ) {
-			$this->harga = $harga; // harga setelah diskon 
-		}
+	public function setHarga( $harga ) {
+		$this->harga = $harga; // harga setelah diskon 
+	}
 
-		public function getHarga() {
-			return $this->harga - ( $this->harga * $this->diskon / 100 ); // bisa diakses di kelas produk beserta kelas turunanya. Harga setelah diberi diskon
-		}
+	public function getHarga() {
+		return $this->harga - ( $this->harga * $this->diskon / 100 ); // bisa diakses di kelas produk beserta kelas turunanya. Harga setelah diberi diskon
+	}
 
-		public function getLabel() {
-			return "$this->penulis, $this->penerbit"; // method
-		}
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit"; // method
+	}
 
-		abstract public function getInfo();
+	abstract public function getInfo();
 
 	}
 
