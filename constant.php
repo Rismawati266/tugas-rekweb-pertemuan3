7560f3b9cb9dd3ce03b60e05878fe3cab43320d6
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 13:02:38 +0700

    memasukkan cost ke dalam kelas dan menampilkan

--- constant.php
+++ constant.php
@@ -1,11 +1,17 @@
 <?php 
 
-	define('NAMA', 'Sandika Galih'); // constanta nama dan nilainya sandika galih
-	echo NAMA;
+	// define('NAMA', 'Sandika Galih'); // constanta nama dan nilainya sandika galih
+	// echo NAMA; // tidak bisa di simpan di dalam kelas.
 
-	echo "<br>";
+	// echo "<br>";
 
-	const UMUR = 32;
-	echo UMUR;
+	// const UMUR = 32; // gapapa bisa simpan di kelas
+	// echo UMUR;
+
+	class Coba {
+		const NAMA = 'Sandika';
+	}
+
+	echo Coba::NAMA;
 
  ?>
