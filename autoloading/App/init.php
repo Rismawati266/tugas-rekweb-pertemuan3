0336ea2909987ac1696ef9f5a7c9af3bd5c80dbd
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 15:36:46 +0700

    menambahkan file init.php

---
+++ autoloading/App/init.php
@@ -0,0 +1,9 @@
+<?php 
+
+	require_once 'App/Produk/InfoProduk.php';
+	require_once 'App/Produk/Produk.php';
+	require_once 'App/Produk/Komik.php'; // untuk satu kelas komik
+	require_once 'App/Produk/Game.php';
+	require_once 'App/Produk/CetakInfoProduk.php';
+
+ ?>
--- autoloading/index.php
+++ autoloading/index.php
@@ -1,12 +1,5 @@
 <?php 
 
-	require_once 'App/Produk/InfoProduk.php';
-	require_once 'App/Produk/Produk.php';
-	require_once 'App/Produk/Komik.php'; // untuk satu kelas komik
-	require_once 'App/Produk/Game.php';
-	require_once 'App/Produk/CetakInfoProduk.php';
-
-	
 	$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 	$produk2 = new Game("Uncharted", "Nail Druckmann", "Sony Computer", 250000, 50);
 
