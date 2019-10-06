ae3ddc07c439f6754282111932f4dec2c9aae67b
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 14:15:34 +0700

    memperbaiki pesan error pada instansiasi cetakProduk

--- abstract-class.php
+++ abstract-class.php
@@ -116,7 +116,7 @@
 	$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 	$produk2 = new Game("Uncharted", "Nail Druckmann", "Sony Computer", 250000, 50);
 
-	$cetakProduk = new cetakProduk();
+	$cetakProduk = new cetakInfoProduk();
 	$cetakProduk->tambahProduk( $produk1 );
 	$cetakProduk->tambahProduk( $produk2 );
 	echo $cetakProduk->cetak(); // menampilkan dua produk sekaligus
