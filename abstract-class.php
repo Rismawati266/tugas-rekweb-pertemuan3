2b2e11955e8ce3fb85fc1f2523a54ad7c5b2e312
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-06 14:08:11 +0700

    memasukkan perintah di function cetak

--- abstract-class.php
+++ abstract-class.php
@@ -105,7 +105,10 @@
 		public function cetak(){ // method cetak
 			$str = "DAFTAR PRODUK : <br>";
 
-			
+			foreach( $this->daftarPrduk as $p ) { // dari daftarProduk kita ambil objek peoduk satu persatu
+				$str .= "- {$p->getInfoProduk()} <br>"; // kita akan bangun stringnya. nampilin info secara lengkap
+			}
+
 			return $str;
 		}
 	}
